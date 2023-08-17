8ebe2f68231cb4952872c31c1061586b0ff4c822
Dominik Croci <dominikrcroci@gmail.com>
2023-08-17 11:44:30 -0700

    added react fragments

--- src/index.js
+++ src/index.js
@@ -68,12 +68,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name}></Pizza>
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name}></Pizza>
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on our menu. Please come back later. </p>
       )}
